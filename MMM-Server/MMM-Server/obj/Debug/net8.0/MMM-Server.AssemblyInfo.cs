fd38d9d3f42d15cbd58e3e45347567055cac7a03
cann-alberto <cann.alberto91@gmail.com>
2026-04-08 10:06:06 +0200

    API deployed on Azure API service Models still compatible with Unity implementation

--- MMM-Server/MMM-Server/obj/Debug/net8.0/MMM-Server.AssemblyInfo.cs
+++ MMM-Server/MMM-Server/obj/Debug/net8.0/MMM-Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MMM-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8f97abfbd7a4118714959c72cd687377da3b88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9d19fc81c80d164c0f73f199afad03ee69b12f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MMM-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MMM-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
